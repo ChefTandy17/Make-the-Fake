0921ec20a848a15df92ca17fce204d919ea4c09e
ChefTandy17 <ttandy@ucsc.edu>
2025-02-20 10:16:10 -0800

    creating the basic structure of the game. some of the functions or files may be added or remove as the project continues to progress

---
+++ scenes/play.js
@@ -0,0 +1,22 @@
+class Play extends Phaser.Scene {
+    constructor() {
+        super('playScene')
+    }
+
+    init(){
+
+    }
+
+    preload(){
+
+    }
+
+    create() {
+
+    }
+  
+    update() {
+
+    }
+
+}
---
+++ src/main.js
@@ -0,0 +1,23 @@
+//Tyvin Tandy
+//Pro Football 1861
+
+
+let config = {
+    type: Phaser.AUTO,
+    width: 1000,
+    height: 500,
+    render: {
+        pixelArt: true 
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {                  
+            debug: true //to set debug mode on or off             
+        },
+    },
+    scene: [ Play ]
+};
+
+//set up the game
+let game = new Phaser.Game(config);
+  
