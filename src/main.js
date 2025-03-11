4ed0a59d6a780584a3e079376cf77491cfb24890
ChefTandy17 <ttandy@ucsc.edu>
2025-03-11 11:55:54 -0700

    added a technical exectuion and debug mode information for graders and ta to grade my work.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
 //Tyvin Tandy
 //Pro Football 1861
 
+// DEBUG MODE: D-KEY
+
+// TECHNICAL EXECUTION:
+// The game uses these Phaser Major components...
+// Particle Effects (Ex: Successfully kicking the football would display a small particle effect)
+// Timer Events (Ex: In playKicker.js and playQB.js, there are timer events which triggers moving the hitbox and display an animation)
+// Input Systems (Ex: Pressing space to kick, enter to throw the football, 1,2,3 buttons to navigate the menu and select screen, etc)
+// Physics System (Arcade Physics) (Ex: The football moves left, and when kicked, moves up. The velocity can also be adjusted if playing as a quarterback)
+// Audio (Ex: Sound is played when the kicker or the quarterback scores)
+// Animaition Manager (Ex: The football in the main menu plays a looping animation, while the qb and kicker have only a one frame animation)
+
 // you need this 
 'use strict'
 
